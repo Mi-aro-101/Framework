24418b09a1e7d226ee3a55aaa4f3186aa638626a
Mi-aro-101 <miaroramanandraitsiory@gmail.com>
2023-03-17 16:05:52 -0400

    Class Scanner done successfully

--- src/java/etu2020/framework/annotation/MethodAnnotation.java
+++ src/java/etu2020/framework/annotation/MethodAnnotation.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/AnnotationType.java to edit this template
  */
-package etu2020.Framework.annotation;
+package etu2020.framework.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
--- src/java/etu2020/framework/servlet/FrontServlet.java
+++ src/java/etu2020/framework/servlet/FrontServlet.java
@@ -5,13 +5,20 @@
 package etu2020.framework.servlet;
 
 import etu2020.framework.Mapping;
+import etu2020.framework.myutils.MyUtils;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,7 +26,7 @@ import java.util.HashMap;
  */
 public class FrontServlet extends HttpServlet {
     
-    HashMap<String, Mapping> urlMapping;
+    HashMap<String, Mapping> urlMapping = new HashMap<>();
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -29,13 +36,25 @@ public class FrontServlet extends HttpServlet {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
+     * @throws java.lang.ClassNotFoundException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, ClassNotFoundException, URISyntaxException {
         response.setContentType("text/html;charset=UTF-8");
         response.setContentType("text/plain");
         PrintWriter out = response.getWriter();
+        
         out.println("Coucou");
+        String packageName = "etu2020";
+        
+        try{
+            ArrayList<Class<?>> allclasses = MyUtils.getClasses(packageName, out);
+            for(Class<?> classes : allclasses){
+                out.println(classes);
+            }
+        }catch(ClassNotFoundException e){
+            out.print(e);
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -50,7 +69,11 @@ public class FrontServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(FrontServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -64,7 +87,11 @@ public class FrontServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(FrontServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
