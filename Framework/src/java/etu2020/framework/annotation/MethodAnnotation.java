25f0116cf206e65f60b32fbe74daebc4d63b613e
Mi-aro-101 <miaroramanandraitsiory@gmail.com>
2023-03-21 15:14:59 +0300

    sprint4

---
+++ Framework/src/java/etu2020/framework/annotation/MethodAnnotation.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/AnnotationType.java to edit this template
+ */
+package etu2020.framework.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author miaro
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface MethodAnnotation {
+    public String url() default "";
+}
